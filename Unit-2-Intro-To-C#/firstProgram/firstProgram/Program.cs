62958422b4fce2de7405585c39b6f85ca2ac9b8d
sean carter <ajacks75@emich.edu>
2025-03-27 22:06:48 -0400

    FirstProp

---
+++ Unit-2-Intro-To-C#/firstProgram/firstProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace firstProgram;
+//this program will ask for three numbers add them together and show the number
+//solution is what you convert to the program lang.
+//a var is used when we need to store something
+//i need a way to do something thats a method 
+//class is the object and funcs are methods inside of the class like a box
+//
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Start of Program");
+        
+        string aLine;
+        aLine = Console.ReadLine();
+        Console.WriteLine("End Of Program");
+    }
+}
---
+++ classnotes/Project1st/Project1st/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Project1st;
+//namespace identifies a context in which a word is known
+//allows the same name to be used in different context to mean different things
+// everything in CSharp is defined in a Class
+// A class is a group of related things
+//a class starts with the class keyword
+class Program
+//{} surround blocks of related code aka code blocks
+//Csharp is a object oreinted programing
+//STart of app is main()
+//a method is a set of code that performs a 
+// Console is an object by c# to represents the screen
+//an object has certain behaviors 
+// A behavior is something ypu do with data
+//behaviors are implemented using methods
+
+//object.method(data) - OOP- object oreinted programing
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Kaizen");
+    }
+}
