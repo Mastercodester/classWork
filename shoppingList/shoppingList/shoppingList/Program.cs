6d38f1389228e871d7911c05368fab657f353356
sean carter <ajacks75@emich.edu>
2025-04-21 18:48:42 -0400

    4/21

--- shoppingList/shoppingList/shoppingList/Program.cs
+++ shoppingList/shoppingList/shoppingList/Program.cs
@@ -56,11 +56,15 @@ class Program
         
 
         Console.WriteLine("What item would you like to buy?");
-        userPick = Console.ReadLine();
-
+       
+       
+            
+        
         switch (userPick)
         {
+        
 
+            
             /*
             Item            Price
             ==============================
@@ -73,6 +77,7 @@ class Program
             grapefruit      $1.99
             honeydew        $3.49
             */
+            
 
             case "apple":
                 cart.Add(new KeyValuePair<string, double>("apple", 0.99));
@@ -110,6 +115,7 @@ class Program
                 Console.WriteLine("Invalid item, please try again.");
                 break;
         }
+     
 
         do
         {
@@ -139,16 +145,16 @@ class Program
     }
     
     
-     /*
-     do 
-            {
-                // Ask the user for the student name and grade
-                Console.Write("Enter student name: ");
-                string studentName = Console.ReadLine(); // Get student name// Ask the user for the student name
-                //Console.Write("Enter grade: ");
-                // Because the user might enter n non-numeri grade causing an exception
-                // We should handle the exception so the program doesn't end
-                //    with a message that will scare the human
+     
+     // /*do 
+     //        {
+     //            // Ask the user for the student name and grade
+     //            Console.Write("Enter student name: ");
+     //            string studentName = Console.ReadLine(); // Get student name// Ask the user for the student name
+     //            //Console.Write("Enter grade: ");
+     //            // Because the user might enter n non-numeri grade causing an exception
+     //            // We should handle the exception so the program doesn't end
+     //            //    with a message that will*/ scare the human
                 // We can use a try/catch to handle the exception
                 // When the user enters a non-numeric grade:
                 //      1. ignore it and keep going - We are doing this
@@ -157,53 +163,53 @@ class Program
                 //      3. Set the grade to 0
 
                 // We need to define grade outside any block so it can be used by all code
-                double grade = 0;
-                string userGrade = "";
-                List<double> grades = new List<double>();
-                try {  // A statement inside this try block might cause an exception
-                    // We need to get multiple grades for each student and store themin a List
-                    string whatTheyTyped = "";
-                    while (whatTheyTyped != "end") // Loop until the user enter "end"
-                    {
-                        Console.Write("Enter grade or end: ");
-                        // Get the user input as a string in case we need it later
-                        whatTheyTyped = Console.ReadLine();
-                        if (whatTheyTyped == "end")
-                        {
-                            break; // exit the loop - continue would be OK too
-                        }
-                        grade = Double.Parse(whatTheyTyped); // convert the user input to number
-                        grades.Add(grade); // Add the grade entered to list
-                    }
-                    // at then end of this look the grades list has all the grade
-                }
-                catch (FormatException exceptionObject) // if it throws a FormatException ...
-                {
-                    Console.WriteLine("The data you entered ("+ userGrade + ") is not a valid number");
-                    Console.WriteLine("The data is ignored");
-                    // Skip adding the student to the dictionary 
-                    continue;  // skip the rest of the loop processing
-                }
-
-                // Add the data to our Dictionary only if we have a valid grade
-                // Dictionary[key}     = value;
-                gradeBook[studentName] = grades; // adding the list of grades for the student
-                // We need to find out if they have more students to enter
-                // We want to be sure they only enter responses we expect
-                // Loop until we get a valid response...
-                // for-loop - Do we know how many times we want to loop? NO
-                // while    - Do we loop based on a condition? YES - Can we loop 0 times? - NO
-                // do-while - Do we loop based on a condition? YES - Do we need loop at least once? - YES
-                do
-                {
-                    Console.WriteLine("Are you done? (y/n)");
-                    // Get a response from the user and convert to lowercase 
-                    userResponse = Console.ReadLine().ToLower();
-                    // We are checking userResponse AFTER we get it from the user
-                } while (userResponse != "y" && userResponse != "n");
-                
-            } while (userResponse != "y");  // Loop while they are not done (done == "y")
-            */
+//                 double grade =/* 0;
+//                 string userGrade = "";
+//                 List<double> grades = new List<double>();
+//                 try {  // A statement inside this try block might cause an exception
+//                     // We need to get multiple grades for each student and store themin a List
+//                     string whatTheyTyped = "";
+//                     while (whatTheyTyped != "end") // Loop until the user enter "end"
+//                     {
+//                         Console.Write("Enter grade or end: ");
+//                         // Get the user input as a string in case we need it later
+//                         whatTheyTyped = Console.ReadLine();
+//                         if (whatTheyTyped == "end")
+//                         {
+//                             break; // exit the loop - continue would be OK too
+//                         }
+//                         grade = Double.Parse(whatTheyTyped); // convert the user input to number
+//                         grades.Add(grade); // Add the grade entered to list
+//                     }
+//                     // at then end of this look the grades list has all the grade
+//                 }
+//                 catch (FormatException exceptionObject) // if it throws a FormatException ...
+//                 {
+//                     Console.WriteLine("The data you entered ("+ userGrade + ") is not a valid number");
+//                     Console.WriteLine("The data is ignored");
+//                     // Skip adding the student to the dictionary 
+//                     continue;  // skip the rest of the loop processing
+//                 }
+//
+//                 // Add the data to our Dictionary only if we have a valid grade
+//                 // Dictionary[key}     = value;
+//                 gradeBook[studentName] = grades; // adding the list of grades for the student
+//                 // We need to find out if they have more students to enter
+//                 // We want to be sure they only enter responses we expect
+//                 // Loop until we get a valid response...
+//                 // for-loop - Do we know how many times we want to loop? NO
+//                 // while    - Do we loop based on a condition? YES - Can we loop 0 times? - NO
+//                 // do-while - Do we loop based on a condition? YES - Do we need loop at least once? - YES
+//                 do
+//                 {
+//                     Console.WriteLine("Are you done? (y/n)");
+//                     // Get a response from the user and convert to lowercase 
+//                     userResponse = Console.ReadLine().ToLower();
+//                     // We are checking userResponse AFTER we get it from the user
+//                 } while (userResponse != "y" && userResponse != "n");
+//                 
+//             } while (userResponse != "y");  // Loop while they are not done (done == "y")*/
+            
     
     
 }
