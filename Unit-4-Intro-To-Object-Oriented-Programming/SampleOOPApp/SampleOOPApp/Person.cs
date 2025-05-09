dc6d1472d1754369f4915d96fdca1ca1f9765790
sean carter <ajacks75@emich.edu>
2025-05-08 22:03:48 -0400

    5/9.end

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApp/SampleOOPApp/Person.cs
@@ -0,0 +1,23 @@
+namespace SampleOOPApp;
+
+
+//
+//THis class will represent a person
+//
+//Attributes: Name
+//              Weight
+//            Height
+//              Age
+//              Gender
+//              EyeColor
+//              HairColor
+//
+public class Person
+{
+    private string _name;
+}
+
+
+public Person(string name){
+    _name = name;
+}
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApp/SampleOOPApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleOOPApp;
+
+class Program
+{
+    
+    //will define an manipulate objects
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person aPerson = new Person("John");
+    }
+}
